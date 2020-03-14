01b66e1c5fe626322828394258ac29cb0e7acd0b
Vladimir <imir2020@yandex.ru>
2020-03-14 19:39:10 +0300

    fails from java0

--- src/ex1/BMI.java
+++ src/ex1/BMI.java
@@ -6,25 +6,27 @@ public class BMI {
     public static void main( String[] args ) {
         // bmi( 84, 195 );
         //Первый способ вычисления индекса массы.
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ввод данных:");
-        System.out.println("Введите вес:");
-        double weight = sc.nextDouble();
-        System.out.println("Введите рост:");
-        double height = sc.nextDouble();
-        double i = ( weight / ( height * height ) ) * 10000;
-        if(i < 18.5){
-            System.out.println("Недостаточная масса тела!");
-         }
-        if(i > 18.5 && i < 25){
-             System.out.println("Ваш вес в норме.");
-         }
-         if(i > 25 && i <35 ){
-        System.out.println("Ожирение!");
-        if(i > 35){
-         System.out.println("Резкое ожирение!!!");
+        Scanner sc = new Scanner( System.in );
+        System.out.println( "Ввод данных:" );
+        System.out.println( "Введите вес:" );
+        double weight = sc.nextDouble( );
+        System.out.println( "Введите рост:" );
+        double height = sc.nextDouble( );
+            double i = ( weight / ( height * height ) ) * 10000;
+            if (i < 18.5) {
+                System.out.println( "Недостаточная масса тела!" );
+            }
+            if (i > 18.5 && i < 25) {
+                System.out.println( "Ваш вес в норме." );
+            }
+            if (i > 25 && i < 35) {
+                System.out.println( "Ожирение!" );
+            }
+            if (i > 35) {
+                System.out.println( "Резкое ожирение!!!" );
+            }
         }
-    }
+
     // Второй способ вычисления индекса массы.
     //static void bmi( double weight, double height ) {
         //double i = ( weight / ( height * height ) ) * 10000;
@@ -42,4 +44,4 @@ public class BMI {
         //}
 
     }
-}
+
